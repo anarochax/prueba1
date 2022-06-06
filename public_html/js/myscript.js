edbe86ffec0240cbe1bc18b2885e2c14859a9860
anarochax <anasoledad.rocha@gmail.com>
2022-06-06 01:29:17 -0300

    probandoPat

---
+++ public_html/js/myscript.js
@@ -0,0 +1,7 @@
+var primera = 40;
+var segunda = 16;
+var sumatotal = primera + segunda;
+var mensaje = "El resultado de la suma de las dos variables es: ";
+document.write("<h1>" +mensaje +Number(sumatotal) +"</h1>");
+
+
